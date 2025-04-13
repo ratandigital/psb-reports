087129f4160d31c6df817dc4341df328ed44cdee
Ratan Kumar Baidya <90468456+ratandigital@users.noreply.github.com>
2025-04-13 11:46:40 +0600

    Update ReportLinks.tsx

--- components/ReportLinks.tsx
+++ components/ReportLinks.tsx
@@ -44,6 +44,18 @@ export function ReportLinks({ isApproved, isManagement, isAdmin }: ReportLinksPr
             >
               Admin List
             </button>
+              <button
+            onClick={() => openInNewWindow('https://psb-report-kappa.vercel.app/interest-calculator')}
+            className="block px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-md"
+          >
+            Additional Interest Voucher
+          </button>
+          <button
+            onClick={() => openInNewWindow('https://psb-reports.vercel.app/interest-calculator')}
+            className="block px-4 py-2 text-gray-700 hover:bg-gray-100 rounded-md"
+          >
+            Aditional Interrest Calculator
+          </button>
           </>
         )}
       </div>
